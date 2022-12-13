e9b120ca469839a6ad8bff07e960e3b63c1de057
Ilian Rusev <ilianrusev@gmail.com>
2022-12-13 21:17:59 +0200

    move main to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
-import { MainComponent } from './main/main.component';
-import { AboutSectionComponent } from './main/components/about-section/about-section.component';
-import { FeaturesSectionComponent } from './main/components/features-section/features-section.component';
+import { MainComponent } from './components/main/main.component';
+import { AboutSectionComponent } from './components/main/components/about-section/about-section.component';
+import { FeaturesSectionComponent } from './components/main/components/features-section/features-section.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, MainComponent, AboutSectionComponent, FeaturesSectionComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MainComponent,
+    AboutSectionComponent,
+    FeaturesSectionComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
