bd39ec53edc48eed3f634125a5515ba7517ebf74
Ilian Rusev <ilianrusev@gmail.com>
2022-12-13 22:07:02 +0200

    features section with cards and build for github

--- src/app/components/main/components/features-section/features-section.component.ts
+++ src/app/components/main/components/features-section/features-section.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-features-section',
   templateUrl: './features-section.component.html',
-  styleUrls: ['./features-section.component.scss']
+  styleUrls: ['./features-section.component.scss'],
 })
 export class FeaturesSectionComponent implements OnInit {
+  cards: any[] = [
+    {
+      title: 'Explore the World',
+      description:
+        'Every explorer is a true adventurer bound by a common curiosity for the unknown.',
+      iconClass: 'icon-basic-world',
+    },
+    {
+      title: 'Meet Nature',
+      description: 'Meet nature and share your experience with other people.',
+      iconClass: 'icon-basic-compass',
+    },
+    {
+      title: 'Find Your Way',
+      description: 'Find your way around the world with our map.',
+      iconClass: 'icon-basic-map',
+    },
+    {
+      title: 'Live A Healthier Life',
+      description: 'Have a healthier lifestyle with our help.',
+      iconClass: 'icon-basic-heart',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
