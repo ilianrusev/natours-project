0a594abdf3024f36a4743d3649c7b35923811189
Ilian Rusev <ilianrusev@gmail.com>
2022-12-20 13:28:18 +0200

    booking section with basic form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AboutSectionComponent } from './components/main/components/about-sectio
 import { FeaturesSectionComponent } from './components/main/components/features-section/features-section.component';
 import { ToursSectionComponent } from './components/main/components/tours-section/tours-section.component';
 import { StoriesSectionComponent } from './components/main/components/stories-section/stories-section.component';
+import { FormComponent } from './components/form/form.component';
+import { BookingSectionComponent } from './components/main/components/booking-section/booking-section.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,8 @@ import { StoriesSectionComponent } from './components/main/components/stories-se
     FeaturesSectionComponent,
     ToursSectionComponent,
     StoriesSectionComponent,
+    FormComponent,
+    BookingSectionComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
