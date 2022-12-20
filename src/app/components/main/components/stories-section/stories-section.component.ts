a70acc50e2cc123b6bdd9e4b60b6dee51eef9fa3
Ilian Rusev <ilianrusev@gmail.com>
2022-12-20 12:39:18 +0200

    complete stories styling

--- src/app/components/main/components/stories-section/stories-section.component.ts
+++ src/app/components/main/components/stories-section/stories-section.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stories-section',
   templateUrl: './stories-section.component.html',
-  styleUrls: ['./stories-section.component.scss']
+  styleUrls: ['./stories-section.component.scss'],
 })
 export class StoriesSectionComponent implements OnInit {
+  stories: any[] = [
+    {
+      title: 'I had the best week ever with my family',
+      description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+      tempora tempore veritatis neque corporis nostrum sed quia labore
+      libero! Consequuntur, sunt minima saepe nostrum distinctio repellat
+      accusamus provident aspernatur sed sunt minima saepe nostrum
+      distinctio!`,
+      imageUrl: 'assets/img/nat-8.jpg',
+      fullName: 'Skyler White',
+    },
+    {
+      title: 'WOW! My life is completely different now',
+      description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+      tempora tempore veritatis neque corporis nostrum sed quia labore
+      libero! Consequuntur, sunt minima saepe nostrum distinctio repellat
+      accusamus provident aspernatur sed sunt minima saepe nostrum
+      distinctio!`,
+      imageUrl: 'assets/img/nat-9.jpg',
+      fullName: 'Walter White',
+    },
+  ];
 
-  constructor() { }
+  backgroundVideoSources: string[] = [
+    'assets/img/video.mp4',
+    'assets/img/video.webm',
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
